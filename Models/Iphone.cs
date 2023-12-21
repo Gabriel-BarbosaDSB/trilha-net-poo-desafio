42394e442ae173ba1d8526b21a67b369184a5ba9
Gabriel Barbosa <gabriel.brbs84@gmail.com>
2023-12-21 14:41:51 -0300

    Resolução de desafio e correção de bugs de versão do dotnet no csproj

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, string memoria) : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o app '{nomeApp}' em Iphone");         
+        }
     }
 }
